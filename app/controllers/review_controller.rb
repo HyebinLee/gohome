e2362fceb65bf045f575ea98cfa6f1d1c215e209
HyebinLee <fional@naver.com>
2015-05-22 10:24:10 -0700

    review

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -8,7 +8,9 @@ class ReviewController < ApplicationController
   def new
     @review = Review.new
   end
-
+  def edit
+    @article=Review.find(params[:id])
+  end
   def create
     @review=Review.new(review_params)
   
@@ -19,6 +21,21 @@ class ReviewController < ApplicationController
     end
   end
 
+  def update
+    @review=Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to @review
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @review=Review.find(params[:id])
+    @review.destroy
+  
+    redirect_to review_index_path
+  end
   private
     def review_params
       params.require(:review).permit(:num, :product, :subject, :text, :name, :date, :star)
