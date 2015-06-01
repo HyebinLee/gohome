e079f8551f82dce9c3d8621c0301981360b68c52
HyebinLee <fional@naver.com>
2015-06-01 06:08:04 -0700

    wow

--- app/controllers/first_controller.rb
+++ app/controllers/first_controller.rb
@@ -1,4 +1,10 @@
 class FirstController < ApplicationController
+  def index
+    @join=Join.all
+    if @join.empty?
+      session[:user_id]=nil
+    end
+  end
   def logout
     session[:user_id] = nil
     redirect_to root_path
